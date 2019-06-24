a85c4b32770d4338ef571021d378b1b2ac39a9a8
dineshsidd <46982101+dineshsidd@users.noreply.github.com>
2019-06-23 21:08:28 -0700

    test

--- Choropleth/static/js/logic.js
+++ Choropleth/static/js/logic.js
@@ -1,8 +1,8 @@
 // Creating map object
-var polygon_data ;
+
 var myMap = L.map("map", {
   center: [0, 0],
-  zoom: 3
+  zoom: 1.5
 });
 
 // Adding tile layer
@@ -22,8 +22,9 @@ var url = "https://raw.githubusercontent.com/datasets/geo-countries/master/data/
 
 
 d3.json(url , function(data){
-  polygon_data = data;
-
+  var polygon_data= {} ;
+   polygon_data.type = data.type;
+   polygon_data.features = [];
 var co2_data ;
 d3.csv("static/js/co2.csv", function(csv_data){
  co2_data= csv_data.map(function(x){
@@ -36,123 +37,46 @@ var geo_row ;
 var final_data = [];
 for ( x=0 ; x< co2_data.length ; x++ ){
   //polygon_data.filter(  )
-console.log(co2_data[x]["Country Code"]);
 geo_row={};
-var geo_tab = polygon_data.features.filter(a=>a.properties.ISO_A3 = co2_data[x]["Country Code"] );
+/*var geo_tab = polygon_data.features.filter(a=>a.properties.ISO_A3 = co2_data[x]["Country Code"] );
 if(geo_tab){
 geo_row = geo_tab[0];
 };
 geo_row.properties.co2_value = co2_data[x]["2014"];
 final_data.push(geo_row);
 }
-
-polygon_data.features = final_data;
-console.log(polygon_data);
-
-//L.geoJson(polygon_data).addTo(myMap);
-
-// Our style object
-var mapStyle = {
-  color: "white",
-  fillColor: "pink",
-  fillOpacity: 0.5,
-  weight: 1.5
-};
-
-console.log(myMap);
-  // Creating a geoJSON layer with the retrieved data
-  L.geoJson(polygon_data, {
-    // Passing in our style object
-    style: mapStyle
-  }).addTo(myMap);
-
-console.log(myMap);           
-});
-
-
-});
-
-
-
-/*
-geojson = L.choropleth(polygon_data, {
-
-  // Define what  property in the features to use
-  valueProperty: "co2_value",
-
-  // Set color scale
-  scale: ["#ffffb2", "#b10026"],
-
-  // Number of breaks in step range
-  steps: 10,
-
-  // q for quartile, e for equidistant, k for k-means
-  mode: "q",
-  style: {
-    // Border color
-    color: "#fff",
-    weight: 1,
-    fillOpacity: 0.8
-  },
-
-  // Binding a pop-up to each layer
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup("<br>Median Household Income:<br>" +
-      "$" + feature.properties.co2_value);
-  }
-}).addTo(myMap);
-
-// Set up the legend
-var legend = L.control({ position: "bottomright" });
-legend.onAdd = function() {
-  var div = L.DomUtil.create("div", "info legend");
-  var limits = geojson.options.limits;
-  var colors = geojson.options.colors;
-  var labels = [];
-
-  // Add min & max
-  var legendInfo = "<h1>Median Income</h1>" +
-    "<div class=\"labels\">" +
-      "<div class=\"min\">" + limits[0] + "</div>" +
-      "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-    "</div>";
-
-  div.innerHTML = legendInfo;
-
-  limits.forEach(function(limit, index) {
-    labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-  });
-
-  div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-  return div;
-};
-
-// Adding legend to the map
-legend.addTo(myMap);
-
-
-
-
-
-
-});
-
-
-});
-
 */
-/*
+if (
+data.features.filter(a=>a.properties.ISO_A3 === co2_data[x]["Country Code"] )[0]){
+let n = +co2_data[x]["2014"];
+data.features.filter(a=>a.properties.ISO_A3 === co2_data[x]["Country Code"] )[0].properties.co2_vlaue = n.toFixed(2);
+polygon_data.features.push(data.features.filter(a=>a.properties.ISO_A3 === co2_data[x]["Country Code"] )[0]);
+}
+}
+
+function getColor(d) {
+  console.log(d);
+  return d > 8 ? '#FF0000' :
+         d > 7 ? '#FF1100' :
+         d > 6 ? '#FF4600' :
+         d > 5 ? '#FF9E00' :
+         d > 4 ? '#FFAF00' :
+         d > 2 ? '#FFC100' :
+         d > 0 ? '#E5FF00' :
+                 '#D4FF00';
+};
 
 
+
+console.log(polygon_data);
 // Grab data with d3
-d3.json(APILink, function(data) {
-  // Create a new choropleth layer
-  console.log(data)
 
-  geojson = L.choropleth(data, {
+/*
+  geojson = L.choropleth(polygon_data, {
+   
 
     // Define what  property in the features to use
-    valueProperty: "MHI",
+    valueProperty: "co2_value",
 
     // Set color scale
     scale: ["#ffffb2", "#b10026"],
@@ -162,30 +86,46 @@ d3.json(APILink, function(data) {
 
     // q for quartile, e for equidistant, k for k-means
     mode: "q",
-    style: {
-      // Border color
-      color: "#fff",
-      weight: 1,
-      fillOpacity: 0.8
-    },
-
     // Binding a pop-up to each layer
     onEachFeature: function(feature, layer) {
-      layer.bindPopup(feature.properties.LOCALNAME + ", " + feature.properties.State + "<br>Median Household Income:<br>" +
-        "$" + feature.properties.MHI);
+      layer.bindPopup(feature.properties.ADMIN + ", " + feature.properties.ISO_A3 + "<br>Co2 Levels:<br>" +
+         feature.properties.co2_vlaue);
     }
   }).addTo(myMap);
+*/
 
+  function style(feature) {
+    console.log(feature.properties.co2_vlaue,feature.properties.ADMIN );
+    return {
+        fillColor: getColor(feature.properties.co2_vlaue),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+L.geoJson(polygon_data, {
+  style: style,
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup(feature.properties.ADMIN + ", " + feature.properties.ISO_A3 + "<br>Co2 Levels Per Capita:<br> (metric tons) <br>" +
+       feature.properties.co2_vlaue);
+  }
+
+}).addTo(myMap);
+/*
   // Set up the legend
   var legend = L.control({ position: "bottomright" });
   legend.onAdd = function() {
     var div = L.DomUtil.create("div", "info legend");
-    var limits = geojson.options.limits;
+    var limits = [1,2,3,4,5,6,7,8,9,10] ; // geojson.options.limits;
     var colors = geojson.options.colors;
     var labels = [];
-
+   console.log(limits);
+   console.log(colors);
     // Add min & max
-    var legendInfo = "<h1>Median Income</h1>" +
+    var legendInfo = "<h1>Co2 Levels</h1>" +
       "<div class=\"labels\">" +
         "<div class=\"min\">" + limits[0] + "</div>" +
         "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
@@ -203,5 +143,11 @@ d3.json(APILink, function(data) {
 
   // Adding legend to the map
   legend.addTo(myMap);
+*/
+});
 
-}); */
+
+});
+
+
+
