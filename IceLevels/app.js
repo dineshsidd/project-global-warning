5835b8a41f75ec8e668ac74fcb4869a8ed536c46
C Estrada <46542353+chrisestrada007@users.noreply.github.com>
2019-06-30 17:04:00 -0700

    updated Ice levels Graph
    Please let me know if these needs work to function on the site.

--- IceLevels/app.js
+++ IceLevels/app.js
@@ -1,126 +1,50 @@
-// Define SVG area dimensions
-var svgWidth = 960;
-var svgHeight = 500;
-
-// Define the chart's margins as an object
-var margin = {
-  top: 60,
-  right: 60,
-  bottom: 60,
-  left: 60
-};
-
-// Define dimensions of the chart area
-var chartWidth = svgWidth - margin.left - margin.right;
-var chartHeight = svgHeight - margin.top - margin.bottom;
-
-// Select body, append SVG area to it, and set its dimensions
-var svg = d3.select("body")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
-
-// Append a group area, then set its margins
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-// Load data from seaLevelData.csv
-d3.csv("seaice.csv", function(error, iceData) {
-
-  // Throw an error if one occurs
-  if (error) throw error;
-
-  // Print the milesData
-  console.log(iceData);
-
-  // Format the date and cast the miles value to a number
-  iceData.forEach(function(data) {
-    data.Year = +data.Year;
-    data.Extent = +data.Extent;
-
-  });
-
-  // Set the domain for the xScale function
-  // d3.extent returns the an array containing the min and max values for the property specified
-  var xScale = d3.scaleLinear()
-    .range([0, chartWidth])
-    .domain(d3.extent(iceData, data => data.Year));
-
-  // Configure a linear scale with a range between the chartHeight and 0
-  // Set the domain for the xLinearScale function
-  var yScale = d3.scaleLinear()
-    .range([chartHeight, 0])
-    .domain([0, d3.max(iceData, data => data.Extent)]);
-    
-
-  // Create two new functions passing the scales in as arguments
-  // These will be used to create the chart's axes
-// Create axis functions
-  var bottomAxis = d3.axisBottom(xScale);
-  var leftAxis = d3.axisLeft(yScale);
-
-  // Add x-axis
-  chartGroup.append("g")
-    .attr("transform", `translate(0, ${chartHeight})`)
-    .call(bottomAxis);
-
-  chartGroup.append("g")
-    // Define the color of the axis text
-    .classed("black", true)
-    .call(leftAxis);
-
-  // Configure a drawLine function which will use our scales to plot the line's points
+d3.json("/icelevel", function(out_data){
+  d3.select("#iceSpin").remove();
   var trace1 = {
-    x: [1, 2, 3, 4, 5, 6, 7, 8],
-    y: [10, 15, null, 17, 14, 12, 10, null, 15],
-    mode: 'lines+markers',
-    connectgaps: true
+    x: out_data["x"], 
+    y: out_data["nh"], 
+    line: {width: 0}, 
+    marker: {color: "444"}, 
+    mode: "lines", 
+    name: "Lower Bound", 
+    type: "scatter"
   };
   
   var trace2 = {
-    x: [1, 2, 3, 4, 5, 6, 7, 8],
-    y: [16, null, 13, 10, 8, null, 11, 12],
-    mode: 'lines',
-    connectgaps: true
+    x: out_data["x"], 
+    y: out_data["sh"],
+    fill: "tonexty", 
+    fillcolor: "#006994", 
+    line: {color: "white"}, 
+    mode: "lines", 
+    name: "Measurement", 
+    type: "scatter"
   };
   
   var data = [trace1, trace2];
+  var layout = {
+    showlegend: false, 
+    paper_bgcolor:'rgba(0,0,0,0)',
+    plot_bgcolor:'rgba(0,0,0,0)',
+    title:{ text: "Ice-level Observations ", font:{color :"white"}},
+    xaxis: {
+      gridcolor: 'gray', 
+      tickfont: {color: '#8a95a8' },
+      showgrid: true, 
+      showline: true, 
+      showticklabels: true, 
+      zeroline: false
+    }, 
+    yaxis: {
+      title:{ text: "Sea-Ice Extent (10^6 sq km)", font:{color :'#8a95a8'}},
+      tickfont: {color: '#8a95a8' },
+      gridcolor: "gray", 
+      showgrid: true, 
+      showline: true, 
+      showticklabels: true, 
+      zeroline: false
+    }
+  };
+  Plotly.plot('icelevel', data, layout);
+  });
   
-  // var layout = {
-  //   title: 'Connect the Gaps Between Data',
-  //   showlegend: false
-  // };
-  
-  // Plotly.newPlot('myDiv', data, layout);
-  
-  //------------------------------------------------------------------------------------
-  // var line1 = d3.line()
-  //   .x(data => xScale(data.Year))
-  //   .y(data => yScale(data.Extent));
-
-  // // Append a path for line1
-  // chartGroup.append("path")
-  //   .data([iceData])
-  //   .attr("d", trace1)
-  //   .classed("line blue", true);
-
-  // // Append a path for line1
-  // chartGroup.append("path")
-  //   .data([iceData])
-  //   .attr("d", trace2)
-  //   .classed("line blue", true);
-
-  // Append axes titles
-  chartGroup.append("text")
-  .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + margin.top})`)
-    .classed("x-Axis text", true)
-    .text("Years (1978-2015)");
-
-    // Append axes titles
-  chartGroup.append("text")
-    .attr("transform", `translate(${margin.left- 100}, ${chartHeight/2} )` + `rotate(270)`)
-    .classed("y-Axis text", true)
-    .text("Change in Ice Levels (10^6 sq km)")
-    ;
-    
-});
--- IceLevels/app.py
+++ IceLevels/app.py
@@ -1,53 +1,17 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing
-import datetime as dt # date and time processing functions
-import itertools
-import matplotlib.pyplot as plt # basic plotting 
-import matplotlib.dates as mdates # date processing in matplotlib
-from matplotlib.offsetbox import AnchoredText
-plt.style.use('ggplot') # use ggplot style
-%matplotlib inline 
-
-# read in the data from the provided csv file
-df = pd.read_csv('seaice.csv')
-
-# drop the 'Source Data' column as it obscures more useful columns and doesn't tell us much
-df.drop('Source Data', axis = 1, inplace = True)
-
-# convert the provided 3 column date format to datetime format and set it as the index
-df['Date'] = pd.to_datetime(df[['Year','Month','Day']])
-df.index = df['Date'].values
-
-# split according to hemisphere, as we are expecting different trends for each
-#north = df[df['hemisphere'] == 'north']
-#south = df[df['hemisphere'] == 'south']
-
-#plt.figure(figsize=(9,3))
-#plt.plot(north.index,north['Extent'], label='Northern Hemisphere')
-#plt.plot(south.index,south['Extent'], label='Southern Hemisphere')
-
-# add plot legend and titles
-#plt.legend(bbox_to_anchor=(0., -.362, 1., .102), loc=3, ncol=2, 
-#           mode="expand", borderaxespad=0.)
-#plt.ylabel('Sea ice extent (10^6 sq km)')
-#plt.xlabel('Date')
-#plt.title('Daily sea-ice extent')
-
-# resample raw data into annual averages
-northyear = north['01-01-1979':'31-12-2016'].resample('12M').mean()
-southyear = south['01-01-1979':'31-12-2016'].resample('12M').mean()
-
-# remove the initial and final item as they aer averaged incorrectly (also indexes seem bad)
-northyear = northyear[1:-1]
-southyear = southyear[1:-1]
-
-plt.figure(figsize=(9,3))
-plt.plot(northyear.Year,northyear['Extent'], marker = '.', label='Northern Hemisphere')
-plt.plot(southyear.Year,southyear['Extent'], marker = '.', label='Southern Hemisphere')
-
-# add plot legend and titles
-plt.legend(bbox_to_anchor=(0., -.362, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
-plt.ylabel('Sea ice extent (10^6 sq km)')
-plt.xlabel('Date')
-plt.title('Annual average sea-ice extent')
-plt.xlim(1977, 2016)
+@app.route("/icelevels")
+def get_ice_levels():
+    var/icelevels = pd.read_csv('static/resources/icelevelsData.csv')
+    var/icelevels = var/icelevels[[
+        'Date', 
+        'hemisphere',
+        'Extent'
+        ]]
+    var/icelevels = var/icelevels.dropna()
+    
+    var_out = {
+        "x" : list(var/icelevels['Date']).copy(),
+        "y" : list(var/icelevels['Extent']).copy(),
+        "nh" : list(var/icelevels['hemisphere' == 'north']).copy(),
+        "sh" : list(var/icelevels['hemisphere' == 'south']).copy()
+    }
+    return jsonify(var_out)
